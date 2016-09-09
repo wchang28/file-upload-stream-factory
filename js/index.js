69cb0cc8e217b8c043692d43a461e1149582a16a
wchang28 <wchang28@hotmail.com>
2016-09-09 13:32:04 -0400

    first comment

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 function get(options) {
     return (function (params) {
-        var filePath = options.filePathMaker(params);
-        var ws = fs.createWriteStream(filePath);
-        return { stream: ws, streamInfo: { filePath: filePath } };
+        var filePath = null;
+        if (options && options.filePathMaker && typeof options.filePathMaker === 'function')
+            filePath = options.filePathMaker(params);
+        try {
+            if (!filePath)
+                throw 1;
+            var parsed = path.parse(filePath);
+            var dir = parsed.dir;
+            if (!dir)
+                throw 1;
+            mkdirp.sync(dir);
+            var ws = fs.createWriteStream(filePath);
+            if (!ws)
+                throw 1;
+            return { stream: ws, streamInfo: { filePath: filePath } };
+        }
+        catch (e) {
+            return { stream: null, streamInfo: null };
+        }
     });
 }
 exports.get = get;
--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,8 @@
 import * as busboyPipe from 'busboy-pipe';
 import * as express from 'express';
 import * as fs from 'fs';
+import * as path from 'path';
+import * as mkdirp from 'mkdirp';
 
 export interface FilePathMaker {
     (params: busboyPipe.FilePipeParams): string
@@ -12,8 +14,19 @@ export interface Options {
 
 export function get(options: Options) : busboyPipe.WriteStreamFactory {
     return ((params: busboyPipe.FilePipeParams) : busboyPipe.WriteStreamInfo => {
-        let filePath = options.filePathMaker(params);
-        let ws = fs.createWriteStream(filePath);
-        return {stream: ws, streamInfo: {filePath: filePath}};
+        let filePath:string = null;
+        if (options && options.filePathMaker && typeof options.filePathMaker === 'function') filePath = options.filePathMaker(params);
+        try {
+            if (!filePath) throw 1;
+            let parsed = path.parse(filePath);
+            let dir = parsed.dir;
+            if (!dir) throw 1;
+            mkdirp.sync(dir);
+            let ws = fs.createWriteStream(filePath);
+            if (!ws) throw 1;
+            return {stream: ws, streamInfo: {filePath: filePath}};
+        } catch (e) {
+            return {stream: null, streamInfo: null};
+        }
     });
 }
